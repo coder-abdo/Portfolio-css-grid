161b1ba5c149dd8c92e11cd983ad193ecf773fdc
coder-abdo <a.h2020.2007@gmail.com>
2018-04-27 23:10:19 +0200

    Finished Modal

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const Toggle = (()=>{
-    const _navBtn = document.querySelector('.menu-button');
     const _body = document.body;
-    const _container = document.querySelector('.container');
-    const _nav = document.querySelector('nav');
+    const _container = _body.querySelector('.container');
+    const _nav = _body.querySelector('nav');
+    const _navBtn = _container.querySelector('.menu-button');
+    const _portfolioContainer = _container.querySelector('.portfolio-items');
+
     const toggleNav = () => {
         _container.style.transition = "transform .25s ease-in-out";
         _body.classList.toggle('is-open');
@@ -11,9 +13,38 @@ const Toggle = (()=>{
         _container.style.transition = "0s";
         _body.classList.remove('is-open');
     }
+    const toggleModal = e => {
+        e.preventDefault();
+        const modalToggler = e.target.closest('.portfolio-link');
+        const modal = modalToggler.parentNode.nextElementSibling;
+        const modalBtn = modal.querySelector('.modal-button');
+        if(!modalToggler) return;
+            modal.classList.add('is-open');
+            modal.style.animation = "modalIn .5s forwards";
+            _body.style.overflowY = "hidden";   
+       
+        const closeModal = () => {
+            modal.classList.remove('is-open');
+            _body.style.overflowY = "auto";
+            modal.removeEventListener('animationend', closeModal);
+             
+        }
+        const actionModal = () => {
+            modal.style.animation = "modalOut .5s forwards";
+            modal.addEventListener('animationend',closeModal);
+            
+        }
+        modalBtn.addEventListener('click', actionModal);
+        document.addEventListener('keyup', e => {
+            if(e.keyCode === 27){
+                actionModal();
+            }
+        });
+    }
     function init(){
         _navBtn.addEventListener('click', toggleNav);
         _nav.addEventListener('click', showSections);
+        _portfolioContainer.addEventListener('click', toggleModal);
     }
     return {
         init: init
